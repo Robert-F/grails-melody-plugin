4900e5192ecd02b493bfbacee29cf017d6721da7
Robert Freudenreich <rf@secomba.com>
2015-07-15 11:50:12 +0200

    Upgraded to Grails 2.5.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,6 @@
 grails.project.work.dir = 'target'
 
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
 
     inherits 'global'
@@ -18,7 +19,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+        build ":release:3.1.1", {
             export = false
         }
     }
